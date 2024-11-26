0cc17c14bbd5e06998244fabd41bdb8d22de0ff9
Anastasia <nastusya89pl@gmail.com>
2024-11-26 23:27:11 +0200

    first commit

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const URI = 'mongodb+srv://nastusya89pl:FzjBkRTIzrL6mHfy@anastasia.x0dp8.mongodb.net/?retryWrites=true&w=majority&appName=Anastasia';
+
+mongoose.connect(URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((e) => {
+    console.error(e);
+})
